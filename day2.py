64adbe0acb3cf931bf59938e1ec03ac7c0120942
Ruhaab Alikhan Manjiyani <137204777+ruhaab-alikhan-manjiyani@users.noreply.github.com>
2025-06-24 12:22:05 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,8 @@
+name = "Ruhaab"
+age = 17
+height = 5.2
+is_coding_today = True
+print("My name is", name)
+print("I am", age, "years old.")
+print("My height is", height, "feet.")
+print("Am I coding today?", is_coding_today)
