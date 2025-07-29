d4e82d1da32c0d9afab5bcc70243b8c4f10a5271
Ruhaab Alikhan Manjiyani <137204777+ruhaab-alikhan-manjiyani@users.noreply.github.com>
2025-07-29 12:14:50 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,9 @@
+text = input("Say Something: ").lower()
+if "hello" in text or "hi" in text :
+    print("Hey there! ✌️")
+elif "how are you?" in text:
+    print("I'm doing great, thanks for asking!")
+elif "bye" in text:
+    print("See ya! 👋")
+else:
+    print("Hmm, I didn't get that 🤔")
