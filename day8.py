563d336c6b62ae6de976cd8a3d81647987ce853b
Ruhaab Alikhan Manjiyani <137204777+ruhaab-alikhan-manjiyani@users.noreply.github.com>
2025-07-30 18:26:33 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,6 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+if username == "ruhaab" and password == "0000":
+    print("Access Granted")
+else:
+    print("Access Denied!")
